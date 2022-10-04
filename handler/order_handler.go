fe870a47d063f13681946116a9c870b2ae07d7bb
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-04 23:16:14 +0700

    feat: order handler

--- handler/order_handler.go
+++ handler/order_handler.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) GetOrders(c *gin.Context) {
+
+}
+
+func (h *Handler) CreateOrder(c *gin.Context) {
+
+}
+
+func (h *Handler) UpdateOrder(c *gin.Context) {
+
+}
+
+func (h *Handler) DeleteOrder(c *gin.Context) {
+
+}
