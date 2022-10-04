5423a7d70c6d755ae123e062d6d95dd4a7f15847
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-04 22:58:00 +0700

    feat: item and order repository

--- repository/item_repository.go
+++ repository/item_repository.go
@@ -1,1 +1,67 @@
 package repository
+
+import (
+	"github.com/nabilsea/hacktiv8-assignment-2.git/model"
+	"gorm.io/gorm"
+)
+
+type ItemRepository interface {
+	FindAll() ([]*model.Item, error)
+	FindById(id int) (*model.Item, error)
+	Save(item *model.Item) (*model.Item, error)
+	Update(item *model.Item) (*model.Item, error)
+}
+
+type itemRepository struct {
+	db *gorm.DB
+}
+
+type IRConfig struct {
+	DB *gorm.DB
+}
+
+func NewItemRepository(c *IRConfig) ItemRepository {
+	return &itemRepository{
+		db: c.DB,
+	}
+}
+
+func (r *itemRepository) FindAll() ([]*model.Item, error) {
+	var items []*model.Item
+
+	err := r.db.Find(&items).Error
+	if err != nil {
+		return items, err
+	}
+
+	return items, nil
+}
+
+func (r *itemRepository) FindById(id int) (*model.Item, error) {
+	var item *model.Item
+
+	err := r.db.Where("id = ?", id).Find(&item).Error
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
+
+func (r *itemRepository) Save(item *model.Item) (*model.Item, error) {
+	err := r.db.Create(&item).Error
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
+
+func (r *itemRepository) Update(item *model.Item) (*model.Item, error) {
+	err := r.db.Save(&item).Error
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
--- repository/order_repository.go
+++ repository/order_repository.go
@@ -1,1 +1,67 @@
 package repository
+
+import (
+	"github.com/nabilsea/hacktiv8-assignment-2.git/model"
+	"gorm.io/gorm"
+)
+
+type OrderRepository interface {
+	FindAll() ([]*model.Order, error)
+	FindById(id int) (*model.Order, error)
+	Save(order *model.Order) (*model.Order, error)
+	Update(order *model.Order) (*model.Order, error)
+}
+
+type orderRepository struct {
+	db *gorm.DB
+}
+
+type ORConfig struct {
+	DB *gorm.DB
+}
+
+func NewOrderRepository(c *ORConfig) OrderRepository {
+	return &orderRepository{
+		db: c.DB,
+	}
+}
+
+func (r *orderRepository) FindAll() ([]*model.Order, error) {
+	var orders []*model.Order
+
+	err := r.db.Find(&orders).Error
+	if err != nil {
+		return orders, err
+	}
+
+	return orders, nil
+}
+
+func (r *orderRepository) FindById(id int) (*model.Order, error) {
+	var order *model.Order
+
+	err := r.db.Where("id = ?", id).Find(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (r *orderRepository) Save(order *model.Order) (*model.Order, error) {
+	err := r.db.Create(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
+
+func (r *orderRepository) Update(order *model.Order) (*model.Order, error) {
+	err := r.db.Save(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
