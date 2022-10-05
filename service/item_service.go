bc50bc83656381aea6fceb2b69e8fd15745c2248
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-05 20:53:17 +0700

    feat: create order service

--- service/item_service.go
+++ service/item_service.go
@@ -1,1 +1,53 @@
 package service
+
+import (
+	"github.com/nabilsea/hacktiv8-assignment-2.git/dto"
+	"github.com/nabilsea/hacktiv8-assignment-2.git/helper/custom_error"
+	"github.com/nabilsea/hacktiv8-assignment-2.git/model"
+	r "github.com/nabilsea/hacktiv8-assignment-2.git/repository"
+)
+
+type ItemService interface {
+	CreateItem(input *dto.CreateItemRequest) (*model.Item, error)
+	UpdateItem() (*model.Item, error)
+}
+
+type itemService struct {
+	itemRepository r.ItemRepository
+}
+
+type ISConfig struct {
+	ItemRepository r.ItemRepository
+}
+
+func NewItemService(c *ISConfig) ItemService {
+	return &itemService{
+		itemRepository: c.ItemRepository,
+	}
+}
+
+func (s *itemService) CreateItem(input *dto.CreateItemRequest) (*model.Item, error) {
+	item, err := s.itemRepository.FindByCode(input.ItemCode)
+	if err != nil {
+		return item, err
+	}
+	if item.ItemID != 0 {
+		return item, &custom_error.ItemAlreadyExists{}
+	}
+
+	item.ItemCode = input.ItemCode
+	item.Description = input.Description
+	item.Quantity = input.Quantity
+	item.OrderID = input.OrderID
+
+	item, err = s.itemRepository.Save(item)
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
+
+func (s *itemService) UpdateItem() (*model.Item, error) {
+	return nil, nil
+}
--- service/order_service.go
+++ service/order_service.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"log"
+	"time"
+
+	"github.com/nabilsea/hacktiv8-assignment-2.git/dto"
 	"github.com/nabilsea/hacktiv8-assignment-2.git/model"
 	r "github.com/nabilsea/hacktiv8-assignment-2.git/repository"
 )
@@ -8,22 +12,25 @@ import (
 type OrderService interface {
 	GetOrders() ([]*model.Order, error)
 	GetOrderById() (*model.Order, error)
-	CreateOrder() (*model.Order, error)
+	CreateOrder(input *dto.CreateOrderRequest) (*model.Order, error)
 	UpdateOrder() (*model.Order, error)
 	DeleteOrder() (*model.Order, error)
 }
 
 type orderService struct {
 	orderRepository r.OrderRepository
+	itemService     ItemService
 }
 
 type OSConfig struct {
 	OrderRepository r.OrderRepository
+	ItemService     ItemService
 }
 
 func NewOrderService(c *OSConfig) OrderService {
 	return &orderService{
 		orderRepository: c.OrderRepository,
+		itemService:     c.ItemService,
 	}
 }
 
@@ -35,8 +42,33 @@ func (s *orderService) GetOrderById() (*model.Order, error) {
 	return nil, nil
 }
 
-func (s *orderService) CreateOrder() (*model.Order, error) {
-	return nil, nil
+func (s *orderService) CreateOrder(input *dto.CreateOrderRequest) (*model.Order, error) {
+	// layoutFormat := "2006-01-02T15:04:05-0700"
+	orderedAt, err := time.Parse(time.RFC3339, input.OrderedAt)
+	if err != nil {
+		return &model.Order{}, err
+	}
+
+	order := &model.Order{}
+	order.CustomerName = input.CustomerName
+	order.OrderedAt = orderedAt
+
+	order, err = s.orderRepository.Save(order)
+	if err != nil {
+		return order, err
+	}
+
+	for _, inputItem := range input.Items {
+		inputItem.OrderID = order.OrderID
+		item, err := s.itemService.CreateItem(&inputItem)
+		if err != nil {
+			log.Println(err.Error())
+		} else {
+			order.Items = append(order.Items, *item)
+		}
+	}
+
+	return order, nil
 }
 
 func (s *orderService) UpdateOrder() (*model.Order, error) {
