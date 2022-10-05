59d1a405adc3a84975ff6b15a2e6d947e984fdd0
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-05 20:51:32 +0700

    feat: order and item response dto

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -1,9 +1,12 @@
 package dto
 
+import "github.com/nabilsea/hacktiv8-assignment-2.git/model"
+
 type CreateItemRequest struct {
-	ItemCode    string `json:"itemCode" binding:"required"`
-	Description string `json:"description" binding:"required"`
+	ItemCode    string `json:"itemCode"`
+	Description string `json:"description"`
 	Quantity    uint   `json:"quantity" binding:"required,min=0"`
+	OrderID     uint
 }
 
 type UpdateItemRequest struct {
@@ -11,4 +14,32 @@ type UpdateItemRequest struct {
 	ItemCode    string `json:"itemCode" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	Quantity    uint   `json:"quantity" binding:"required,min=0"`
+	OrderID     uint
+}
+
+type ItemResponse struct {
+	LineItemID  uint   `json:"lineItemId"`
+	ItemCode    string `json:"itemCode"`
+	Description string `json:"description"`
+	Quantity    uint   `json:"quantity"`
+	OrderID     uint   `json:"orderId"`
+}
+
+func FormatItem(item model.Item) ItemResponse {
+	return ItemResponse{
+		LineItemID:  item.ItemID,
+		ItemCode:    item.ItemCode,
+		Description: item.Description,
+		Quantity:    item.Quantity,
+		OrderID:     item.OrderID,
+	}
+}
+
+func FormatItems(items []model.Item) []ItemResponse {
+	formattedItems := []ItemResponse{}
+	for _, item := range items {
+		formattedItem := FormatItem(item)
+		formattedItems = append(formattedItems, formattedItem)
+	}
+	return formattedItems
 }
--- dto/order_dto.go
+++ dto/order_dto.go
@@ -1,15 +1,30 @@
 package dto
 
-import "time"
+import "github.com/nabilsea/hacktiv8-assignment-2.git/model"
 
 type CreateOrderRequest struct {
-	OrderedAt    time.Time           `json:"orderedAt" binding:"required"`
+	OrderedAt    string              `json:"orderedAt" binding:"required"`
 	CustomerName string              `json:"customerName" binding:"required"`
 	Items        []CreateItemRequest `json:"items" binding:"required"`
 }
-
 type UpdateOrderRequest struct {
-	OrderedAt    time.Time           `json:"orderedAt" binding:"required"`
+	OrderedAt    string              `json:"orderedAt" binding:"required"`
 	CustomerName string              `json:"customerName" binding:"required"`
 	Items        []UpdateItemRequest `json:"items" binding:"required"`
+}
+
+type OrderResponse struct {
+	OrderID      uint           `json:"orderId"`
+	OrderedAt    string         `json:"orderedAt"`
+	CustomerName string         `json:"customerName"`
+	Items        []ItemResponse `json:"items"`
+}
+
+func FormatOrder(order *model.Order) OrderResponse {
+	return OrderResponse{
+		OrderID:      order.OrderID,
+		OrderedAt:    order.OrderedAt.String(),
+		CustomerName: order.CustomerName,
+		Items:        FormatItems(order.Items),
+	}
 }
