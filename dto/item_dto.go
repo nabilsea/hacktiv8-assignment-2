5b474dda6da215fbb95444127a71a2b6a4a335d2
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-05 08:55:58 +0700

    feat: order and item request dto

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -1,1 +1,14 @@
 package dto
+
+type CreateItemRequest struct {
+	ItemCode    string `json:"itemCode" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Quantity    uint   `json:"quantity" binding:"required,min=0"`
+}
+
+type UpdateItemRequest struct {
+	LineItemID  uint   `json:"lineItemId" binding:"required"`
+	ItemCode    string `json:"itemCode" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Quantity    uint   `json:"quantity" binding:"required,min=0"`
+}
--- dto/order_dto.go
+++ dto/order_dto.go
@@ -1,1 +1,15 @@
 package dto
+
+import "time"
+
+type CreateOrderRequest struct {
+	OrderedAt    time.Time           `json:"orderedAt" binding:"required"`
+	CustomerName string              `json:"customerName" binding:"required"`
+	Items        []CreateItemRequest `json:"items" binding:"required"`
+}
+
+type UpdateOrderRequest struct {
+	OrderedAt    time.Time           `json:"orderedAt" binding:"required"`
+	CustomerName string              `json:"customerName" binding:"required"`
+	Items        []UpdateItemRequest `json:"items" binding:"required"`
+}
