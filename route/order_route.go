59a5ce5d41cfefb113c4c79732cdfd34f75f22ff
Aulia Ahmad Nabil <aulia.nabil@shopee.com>
2022-10-05 22:17:36 +0700

    refactor: params name in update and delete route

--- route/order_route.go
+++ route/order_route.go
@@ -9,6 +9,6 @@ func (r *Router) Order(route *gin.RouterGroup, h *handler.Handler) {
 	route = route.Group("/orders")
 	route.GET("/", h.GetOrders)
 	route.POST("/", h.CreateOrder)
-	route.PUT("/:id", h.UpdateOrder)
-	route.DELETE("/:id", h.DeleteOrder)
+	route.PUT("/:orderId", h.UpdateOrder)
+	route.DELETE("/:orderId", h.DeleteOrder)
 }
